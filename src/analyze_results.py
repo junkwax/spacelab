bb2e62990a6b53190f6119260d4c99a2f46dc688
JUNKWAX <132296345+junkwax@users.noreply.github.com>
2025-02-15 08:25:15 -0600

    Create analyze_results.py

---
+++ src/analyze_results.py
@@ -0,0 +1,22 @@
+import h5py
+import matplotlib.pyplot as plt
+
+def analyze_fit_results(input_file, plot_dir):
+    """Analyze MCMC fit results."""
+    with h5py.File(input_file, 'r') as f:
+        samples = f['samples'][()]
+    
+    plt.figure()
+    plt.hist(samples[:, :, 0].flatten(), bins=50, label='Mass')
+    plt.hist(samples[:, :, 1].flatten(), bins=50, label='Coupling')
+    plt.legend()
+    plt.savefig(f"{plot_dir}/parameter_distributions.png")
+    plt.close()
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, required=True)
+    parser.add_argument('--plot', type=str, required=True)
+    args = parser.parse_args()
+    analyze_fit_results(args.input, args.plot)
